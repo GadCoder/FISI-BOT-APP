af1004f412a34a86a7455dc9217f3b453404fdc3
Valeria Llerena <valeryjhoanna@hotmail.com>
2024-11-29 11:52:42 -0500

    Chat

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'messages.dart'; 
+import 'messages.dart';
 
 class Chat extends StatefulWidget {
   const Chat({Key? key}) : super(key: key);
@@ -7,11 +7,19 @@ class Chat extends StatefulWidget {
   @override
   State<Chat> createState() => _ChatState();
 }
+
 class _ChatState extends State<Chat> {
   final TextEditingController _controller = TextEditingController();
 
-  // Lista de mensajes y si es userMessage[] 
+  // Lista de mensajes y si es userMessage[]
   List<Map<String, dynamic>> messages = [];
+
+  // Lista de sugerencias
+  final List<String>suggestions = [
+    "¿Cuando termina el semestre?",
+    "¿Noticias sobre la facultad?",
+    "Horarios de atención"
+  ];
 
   @override
   void initState() {
@@ -20,14 +28,23 @@ class _ChatState extends State<Chat> {
     sendWelcomeMessage();
   }
 
-  void sendWelcomeMessage() {
-    setState(() {
-      messages.add({
-        'message': 'Bienvenido! Soy un CHATBOT, puedes preguntarme sobre asuntos relacionados a la Universidad como: Tramites, Profesores y Aulas.',
-        'isUserMessage': false,
-      });
+void sendWelcomeMessage() {
+  setState(() {
+    messages.add({
+      'message': '''
+Bienvenido! Soy FISI-CHATBOT. Aquí hay algunas cosas con las que puedo ayudarte:
+
+- Consultar sobre trámites facultad.
+- Información sobre profesores.
+- Información sobre procesos/actividades que se llevan en la facultad.
+- Otros servicios relacionados con la facultad.
+
+''',
+      'isUserMessage': false,
+      'image': 'assets/images/chatbot.webp', 
     });
-  }
+  });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +57,34 @@ class _ChatState extends State<Chat> {
           Expanded(
             child: MessagesScreen(messages: messages), // Pasar lista de mensajes
           ),
+          // Sugerencias con fondo igual al chat
+          if (suggestions.isNotEmpty)
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 14),
+              color: Theme.of(context).scaffoldBackgroundColor, // Match chat background
+              child: Wrap(
+                spacing: 8.0,
+                runSpacing: 8.0,
+                alignment: WrapAlignment.start,
+                children: suggestions.map((suggestion) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      sendMessage(suggestion);
+                    },
+                    child: Text(suggestion),
+                  );
+                }).toList(),
+              ),
+            ),
+          // Input para escribir mensajes
           Container(
             padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
             color: Colors.white,
@@ -74,13 +119,29 @@ class _ChatState extends State<Chat> {
   // Agregar mensajes de bot y usuarios
   void sendMessage(String text) {
     if (text.isEmpty) return; // No enviar mensajes vacios
-
+     
     setState(() {
       // Agregar mensaje de usuario
       messages.add({'message': text, 'isUserMessage': true});
-
+      String botResponse= 'Me preguntaste: $text';
       // Aqui iria lo de BOT
-      String botResponse = 'Me preguntaste: $text'; // Logica del bot
+      if(text == '¿Cuando atiende la UNAYOE? '){
+        botResponse = '''
+La UNAYOE atiende de 8 am a 4 pm.''';
+      }else if(text == '¿Como está calificado el profesor Guerra de IOT?'){
+        botResponse = '''
+El profesor Guerra enseña IOT y esta calificado con 4 estrellas, por los compañeros.''';
+      }else if(text== '¿Donde puedo realizar el tramite de Solicitud Simple?'){
+       botResponse = '''
+El tramite de Solicitud Simple se encuentra DISPONIBLE. 
+
+Requisitos: 
+    -Solicitud simple especificando a quien va dirigido.
+    -Documento Sustentatorio (OPCIONAL) solo si es necesario.
+
+
+***Si cumples los requisitos puedes realizar siguiendo el siguiente URL: https://tramiteonline.unmsm.edu.pe/sgdfd/mat/tipo-tramite/solicitud-simple?local=20.''';
+      }
       messages.add({'message': botResponse, 'isUserMessage': false});
     });
   }
--- lib/screens/messages.dart
+++ lib/screens/messages.dart
@@ -1,108 +1,270 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/gestures.dart';
 
 class MessagesScreen extends StatefulWidget {
-  final List messages;
+  final List<Map<String, dynamic>> messages;
   const MessagesScreen({Key? key, required this.messages}) : super(key: key);
 
   @override
   _MessagesScreenState createState() => _MessagesScreenState();
 }
+
 class _MessagesScreenState extends State<MessagesScreen> {
   @override
-  Widget build(BuildContext context) {
-    var w = MediaQuery.of(context).size.width;
-    return ListView.separated(
-      itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: widget.messages[index]['isUserMessage']
-                ? CrossAxisAlignment.end
-                : CrossAxisAlignment.start,
-            children: [
+@override
+Widget build(BuildContext context) {
+  var w = MediaQuery.of(context).size.width;
+  return ListView.separated(
+    itemBuilder: (context, index) {
+      // Extract message and optional image from the current message
+      final currentMessage = widget.messages[index];
+      final messageText = currentMessage['message'];
+      final isUserMessage = currentMessage['isUserMessage'];
+      final messageImage = currentMessage['image'];
+
+      return Container(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: isUserMessage
+              ? CrossAxisAlignment.end
+              : CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: isUserMessage
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 14, horizontal: 14),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomRight: Radius.circular(isUserMessage ? 0 : 20),
+                      topLeft: Radius.circular(isUserMessage ? 20 : 0),
+                    ),
+                    color: isUserMessage
+                        ? Colors.white
+                        : Theme.of(context).colorScheme.primary,
+                  ),
+                  constraints: BoxConstraints(maxWidth: w * 2 / 3),
+                  child: _buildMessageContent(
+                    messageText,
+                    isUserMessage,
+                    image: messageImage, // Pasar la imagen dinamicamente
+                  ),
+                ),
+              ],
+            ),
+            // Botones: Reportar, Copiar y Compartir (Solo para mensajes del bot).
+            if (!isUserMessage && index > 0)
               Row(
-                mainAxisAlignment: widget.messages[index]['isUserMessage']
+                mainAxisAlignment: isUserMessage
                     ? MainAxisAlignment.end
                     : MainAxisAlignment.start,
                 children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 14),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                        bottomRight: Radius.circular(
-                            widget.messages[index]['isUserMessage'] ? 0 : 20),
-                        topLeft: Radius.circular(
-                            widget.messages[index]['isUserMessage'] ? 20 : 0),
-                      ),
-                      color: widget.messages[index]['isUserMessage']
-                          ? Colors.white
-                          : Theme.of(context).colorScheme.primary,
-                    ),
-                    constraints: BoxConstraints(maxWidth: w * 2 / 3),
-                    child: Text(
-                      widget.messages[index]['message'],
-                      style: TextStyle(
-                        color: widget.messages[index]['isUserMessage']
-                            ? Colors.black
-                            : Colors.white,
-                      ),
-                    ),
+                  IconButton(
+                    icon: Icon(Icons.report),
+                    onPressed: () {
+                      reportMessage(index);
+                    },
+                  ),
+                  Text('Reportar'),
+                  IconButton(
+                    icon: Icon(Icons.copy),
+                    onPressed: () {
+                      copyMessage(messageText);
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.share),
+                    onPressed: () {
+                      shareMessage(messageText);
+                    },
                   ),
                 ],
               ),
-              // Boton de reportar solo si es el bot y no es el mensaje de bienvenida
-              if (!widget.messages[index]['isUserMessage'] && index > 0)
-                Row(
-                  mainAxisAlignment: widget.messages[index]['isUserMessage']
-                      ? MainAxisAlignment.end
-                      : MainAxisAlignment.start,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.report),
-                      onPressed: () {
-                        reportMessage(index);
-                      },
-                    ),
-                    Text('Reportar'),
-                  ],
-                ),
-            ],
+          ],
+        ),
+      );
+    },
+    separatorBuilder: (_, i) => Padding(padding: EdgeInsets.only(top: 5)),
+    itemCount: widget.messages.length,
+  );
+}
+
+
+Widget _buildMessageContent(String message, bool isUserMessage, {String? image}) {
+  final urlRegExp = RegExp(
+    r'(https?://[^\s]+)',
+    caseSensitive: false,
+  );
+
+  final matches = urlRegExp.allMatches(message);
+
+  List<Widget> content = [];
+
+  // Add text content
+  if (matches.isEmpty) {
+    content.add(
+      Text(
+        message,
+        style: TextStyle(
+          color: isUserMessage ? Colors.black : Colors.white,
+        ),
+      ),
+    );
+  } else {
+    final List<TextSpan> spans = [];
+    int start = 0;
+
+    for (final match in matches) {
+      if (match.start > start) {
+        spans.add(TextSpan(
+          text: message.substring(start, match.start),
+          style: TextStyle(
+            color: isUserMessage ? Colors.black : Colors.white,
           ),
-        );
-      },
-      separatorBuilder: (_, i) => Padding(padding: EdgeInsets.only(top: 5)),
-      itemCount: widget.messages.length,
+        ));
+      }
+
+      final url = message.substring(match.start, match.end);
+      spans.add(
+        TextSpan(
+          text: url,
+          style: TextStyle(
+            color: Colors.blue,
+            decoration: TextDecoration.underline,
+          ),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () => _launchUrl(url),
+        ),
+      );
+
+      start = match.end;
+    }
+
+    if (start < message.length) {
+      spans.add(TextSpan(
+        text: message.substring(start),
+        style: TextStyle(
+          color: isUserMessage ? Colors.black : Colors.white,
+        ),
+      ));
+    }
+
+    content.add(
+      RichText(
+        text: TextSpan(children: spans),
+      ),
     );
   }
 
-  void reportMessage(int index) {
-    // Logic to handle reporting the message
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Reportar'),
-          content: Text('Ayúdanos a mejorar, ¿Esta respuesta fue satisfactoria?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Sí'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('No'),
-            ),
-          ],
-        );
-      },
+// Agregar Imagen (Opcional)
+if (image != null) {
+  content.add(
+    SizedBox(height: 3), // Espacio
+  );
+  content.add(
+    Center(
+      child: Image.asset(
+        image,
+        height: 90,
+        width: 100, // Ajustar Tamaño
+        fit: BoxFit.contain, 
+      ),
+    ),
+  );
+}
+
+
+  return Column(
+    crossAxisAlignment:
+        isUserMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+    children: content,
+  );
+}
+
+
+  void _launchUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('No se pudo ingresar a: $url')),
+      );
+    }
+  }
+
+  void copyMessage(String message) {
+    Clipboard.setData(ClipboardData(text: message));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Mensaje Copiado!')),
     );
   }
+
+  void shareMessage(String message) {
+    Share.share(message);
+  }
+
+  void reportMessage(int index) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      String? selectedReason;
+      TextEditingController textController = TextEditingController();
+      return AlertDialog(
+        title: Text('Reportar Respuesta'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            DropdownButtonFormField<String>(
+              hint: Text('Elija una razón'),
+              value: selectedReason,
+              items: [
+                DropdownMenuItem(value: 'Inadecuado', child: Text('Inadecuado')),
+                DropdownMenuItem(value: 'Impreciso', child: Text('Impreciso')),
+                DropdownMenuItem(value: 'Otro', child: Text('Otro')),
+              ],
+              onChanged: (value) {
+                selectedReason = value;
+              },
+            ),
+            TextField(
+              controller: textController,
+              maxLength: 500,
+              decoration: InputDecoration(
+                hintText: 'Especifique más (máx. 500 caracteres)',
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              // You might want to handle the collected data here
+              String? reason = selectedReason;
+              String comments = textController.text;
+              // Handle the data as needed
+
+              Navigator.of(context).pop();
+            },
+            child: Text('Reportar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 }
